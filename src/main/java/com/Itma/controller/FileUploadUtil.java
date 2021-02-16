c5a89f8255d03a37c9336956b7906838653335e4
manu-prathap <manuprathap09@gmail.com>
2021-02-16 21:21:08 +0530

    Adding Registration Controller

---
+++ src/main/java/com/Itma/controller/FileUploadUtil.java
@@ -0,0 +1,35 @@
+package com.Itma.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUploadUtil{
+
+	public static void saveFile(String uploadDir, String fileName, MultipartFile multiPart) throws IOException {
+		
+		Path uploadPath = Paths.get(uploadDir);
+		
+		 if (!Files.exists(uploadPath)) {
+	            Files.createDirectories(uploadPath);
+	        }
+	         
+	        try (InputStream inputStream = multiPart.getInputStream()) {
+	            Path filePath = uploadPath.resolve(fileName);
+	            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+	        } catch (IOException ioe) {  
+	        	
+	            throw new IOException("Could not save image file: " + fileName, ioe);
+	        
+	        } 
+		
+		
+	}
+	
+}
+	
